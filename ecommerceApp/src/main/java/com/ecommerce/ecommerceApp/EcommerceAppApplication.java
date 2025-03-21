bdbfb5a4946d399aabff582e4d8a6439da0eef7e
Bhargavi Komandoor <bhargavikawshika@gmail.com>
2025-03-21 18:22:52 +0530

    This is first commit

--- ecommerceApp/src/main/java/com/ecommerce/ecommerceApp/EcommerceAppApplication.java
+++ ecommerceApp/src/main/java/com/ecommerce/ecommerceApp/EcommerceAppApplication.java
@@ -8,6 +8,7 @@ public class EcommerceAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceAppApplication.class, args);
+		System.out.println("Hello!!!....This is my springboot ecommerce application");
 	}
 
 }
